9e6e10b108648df5bc8889dbbedb798a8ceb933f
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2022-08-29 17:03:25 -0400

    Initialize empty C++ class in Clion

---
+++ crypt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Azriel on 8/29/2022.
+//
+
+#include "crypt.h"
---
+++ crypt.h
@@ -0,0 +1,14 @@
+//
+// Created by Azriel on 8/29/2022.
+//
+
+#ifndef CRYPTKEEPER_CRYPT_H
+#define CRYPTKEEPER_CRYPT_H
+
+
+class crypt {
+
+};
+
+
+#endif //CRYPTKEEPER_CRYPT_H
